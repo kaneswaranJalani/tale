16ef0055b55e5f94cf62c1c735f611923e85364d
kaneswaranJalani <kaneswaranjalani@gmail.com>
2025-04-22 16:43:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Section from "./components/Section"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+function App() {
+  
+
+  return (
+    <>
+     <Header/> 
+     <Section/>
+     <Footer/>
+     
+    </>
+  )
+}
+
+export default App
